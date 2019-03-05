910fcc4779ac592bbc9b70e1462b3374f1f6ca4e
ehmicky <ehmicky@gmail.com>
2019-03-05 10:00:00 +0100

    Dummy commit

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const keepFuncProps = function(functor) {
     return functor
   }
 
+  if (functor === 'example') {
+    return functor
+  }
+
   return function newFunctor(func, ...args) {
     // eslint-disable-next-line fp/no-this, no-invalid-this
     const newFunc = functor.call(this, func, ...args)
